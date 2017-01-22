efb039a46f0de8d8dc7c4021f3562961720c2204
Luca Ronca <luca.ronca@jcmaxwell.it>
2017-01-22 09:18:34 +0100

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,1 +1,23 @@
+package registroelettronico;
+import java.util.Scanner;
 
+public class Admin {
+    private String nomeUtente = "Admin";
+    private String password;
+    public final String tipoUtente = "Admin";
+    public static final int N_STUDENTI = 24;
+    public Studenti[] classe;
+    public int nStudenti;
+    //Costruttore
+    public Admin(String password){
+        setPassword(password);
+    }
+    //Setter/Getter
+    public void setPassword(String p){
+        password = p;
+    }
+    public String getPassword(){
+        return password;
+    }
+    //Metodi
+}
