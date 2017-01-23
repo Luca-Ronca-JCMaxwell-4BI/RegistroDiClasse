0437c1f20912648867ed9a240eea205d75e527af
Serena-DAmbrosio-JCMAXWELL-4Bi <Serena-DAmbrosio-JCMAXWELL-4Bi@users.noreply.github.com>
2017-01-23 09:44:39 +0000

    Update Admin.java
    aggiunto dei commenti

--- Admin.java
+++ Admin.java
@@ -1,12 +1,12 @@
 package registroelettronico;
-import java.util.Scanner;
-
+import java.util.Scanner; /*importa la classe Scanner la quale è nella cartella UTIL che è dentro la cartella JAVA.  
+                          Scanner : classe predefinita per prendere input dagli utenti*/
 public class Admin {
     private String nomeUtente = "Admin";
     private String password;
     public final String tipoUtente = "Admin";
     public static final int N_STUDENTI = 24;
-    public Studenti[] classe;
+    public Studenti[] classe; //vettore che contiene gli oggetti di tipo Studenti
     public int nStudenti;
     //Costruttore
     public Admin(String password){
