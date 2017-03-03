9d037033f104b6c8b091cfe5fb2ecb917edac330
Serena-DAmbrosio-JCMAXWELL-4Bi <Serena-DAmbrosio-JCMAXWELL-4Bi@users.noreply.github.com>
2017-03-03 10:34:32 +0100

    Update RegistroElettronico.java
    aggiornato classe Registro

--- RegistroElettronico.java
+++ RegistroElettronico.java
@@ -16,21 +16,19 @@ public class RegistroElettronico {
     }
     //Metodi
     public void accesso(Admin admin){
-        int op, i, pos;
-        String nu, pw;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("REGISTRO ELETTRONICO");
-        System.out.println("ver " + this.getVersione());
-        System.out.println("-------------------------");
+        int opa, i, t = 0;
+        String nu, pw, nome, pass;
+        Scanner sc = new Scanner(System.in);        
         do
         {
+            System.out.println("-------------------------");
             System.out.println("Inserire il tipo dell'utente");
             System.out.println("0 - Admin");
             System.out.println("1 - Professore");
             System.out.println("2 - Studente");
             System.out.println("3 - Esci");
-            op = sc.nextInt();
-            switch(op)
+            opa = sc.nextInt();
+            switch(opa)
             {
                 case 0:
                     System.out.println("Inserire password");
@@ -42,7 +40,7 @@ public class RegistroElettronico {
                     }
                     else
                     {
-                        System.out.println("Utente inesistente");
+                        System.out.println("Password errata");
                     }
                     break;
                 case 1:
@@ -50,18 +48,17 @@ public class RegistroElettronico {
                     nu = sc.next();
                     System.out.println("Inserire password");
                     pw = sc.next();
-                    for(i=0; i<admin.getNProfessori(); i++)
+                    for(i=0; i<admin.nProfessori; i++)
                     {
                         if(admin.prof[i].getNomeUtente().equals(nu) && admin.prof[i].getPassword().equals(pw))
                         {
-                            System.out.println("Accesso in corso");
-                            pos = i;
-                            homeProfessori(admin, pos);
+                            homeProfessori(admin, i);
+                            t = 1;
                         }
-                        else
-                        {
-                            System.out.println("Utente inesistente");
-                        }
+                    }
+                    if(t == 0)
+                    {
+                        System.out.println("Nome utente o password errati");
                     }
                     break;
                 case 2:
@@ -69,27 +66,156 @@ public class RegistroElettronico {
                     nu = sc.next();
                     System.out.println("Inserire password");
                     pw = sc.next();
-                    for(i=0; i<admin.getNStudenti(); i++)
+                    for(i=0; i<admin.nStudenti; i++)
                     {
                         if(admin.classe[i].getNomeUtente().equals(nu) && admin.classe[i].getPassword().equals(pw))
                         {
-                            System.out.println("Accesso in corso");
-                            pos = i;
-                            homeStudenti(admin, pos);
+                            homeStudenti(admin, i);
+                            t = 1;
                         }
-                        else
-                        {
-                            System.out.println("Utente inesistente");
-                        }
+                    }
+                    if(t == 0)
+                    {
+                        System.out.println("Nome utente o password errati");
                     }
                     break;
                 case 3:
                     System.out.println("Uscita in corso ...");
                     System.out.println("-------------------------");
+                    break;
                 default:
                     System.out.println("Tipologia di utente inesistente");
                     break;
             }
         }
-        while(op != 3); 
+        while(opa != 3); 
+    }
+    public void homeAdmin(Admin admin){
+        int opha;
+        Scanner sc = new Scanner(System.in);
+        do
+        {
+            System.out.println("-------------------------");
+            System.out.println("Scegliere un opzione");
+            System.out.println("0 - Cambia password");
+            System.out.println("1 - Aggiungi professore");
+            System.out.println("2 - Elimina professore");
+            System.out.println("3 - Aggiungi studente");
+            System.out.println("4 - Elimina studente");
+            System.out.println("5 - Esci");
+            opha = sc.nextInt();            
+            switch(opha)
+            {
+                case 0:
+                    admin.cambiaPassword();
+                    break;
+                case 1:
+                    admin.aggiungiProfessore();
+                    break;
+                case 2:
+                    admin.eliminaProfessore();
+                    break;
+                case 3:
+                    admin.aggiungiStudente();
+                    break;
+                case 4:
+                    admin.eliminaStudente();
+                    break;
+                case 5:
+                    System.out.println("Uscita in corso ...");
+                    System.out.println("-------------------------");
+                    break;
+                default:
+                    System.out.println("Opzione non valida");
+                    break;
+            }
+        }
+        while(opha != 5); 
+    }
+    public void homeProfessori(Admin admin, int pos){
+        int ophp;
+        Scanner sc = new Scanner(System.in);
+        do
+        {
+            System.out.println("-------------------------");
+            System.out.println("Scegliere un opzione");
+            System.out.println("0 - Cambia password");
+            System.out.println("1 - Aggiungi voto");
+            System.out.println("2 - Elimina voto");
+            System.out.println("3 - Esci");
+            ophp = sc.nextInt();
+            switch(ophp)
+            {
+                case 0:
+                    admin.prof[pos].cambiaPassword();
+                    break;
+                case 1:
+                    admin.prof[pos].aggiungiVoto(admin);
+                    break;
+                case 2:
+                    admin.prof[pos].eliminaVoto(admin);
+                    break;
+                case 3:
+                    System.out.println("Uscita in corso ...");
+                    System.out.println("-------------------------");
+                    break;
+                default:
+                    System.out.println("Opzione non valida");
+                    break;
+            }
+        }
+        while(ophp != 3); 
+    }
+    public void homeStudenti(Admin admin, int pos){
+        int ophs;
+        Scanner sc = new Scanner(System.in);
+        do
+        {
+            System.out.println("-------------------------");
+            System.out.println("Scegliere un opzione");
+            System.out.println("0 - Cambia password");
+            System.out.println("1 - Visualizza voti");
+            System.out.println("2 - Calcola medie");
+            System.out.println("3 - Calcola stato");
+            System.out.println("4 - Esci");
+            ophs = sc.nextInt();
+            switch(ophs)
+            {
+                case 0:
+                    admin.classe[pos].cambiaPassword();
+                    break;
+                case 1:
+                    admin.classe[pos].visualizzaVoti();
+                    break;
+                case 2:
+                    double mediaTot = (admin.classe[pos].mediaMatematica + admin.classe[pos].mediaItaliano + admin.classe[pos].mediaInformatica) / 3;
+                    System.out.println("Media matematica: " + admin.classe[pos].calcolaMediaMatematica());
+                    System.out.println("Media italiano: " + admin.classe[pos].calcolaMediaItaliano());
+                    System.out.println("Media informatica: " + admin.classe[pos].calcolaMediaInformatica());
+                    System.out.println("Media totale: " + mediaTot);
+                    break;
+                case 3:
+                    System.out.println("Stato studente: " + admin.classe[pos].calcolaStato());
+                    break;
+                case 4:
+                    System.out.println("Uscita in corso ...");
+                    System.out.println("-------------------------");
+                    break;
+                default:
+                    System.out.println("Opzione non valida");
+                    break;
+            }
+        }
+        while(ophs != 4); 
+    }
+
+    public static void main(String[] args) {
+        RegistroElettronico RE;
+        Admin admin;
+        RE = new RegistroElettronico("1.0");
+        admin = new Admin("admin");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("REGISTRO ELETTRONICO");
+        System.out.println("ver " + RE.getVersione());
+        RE.accesso(admin);
     }
